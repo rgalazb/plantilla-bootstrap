2602a39ac1b335f62b4128b9ee0f6fe79038afcb
rgalazb <galaz.becerra@gmail.com>
2018-02-13 17:00:07 -0300

    font awesome agregado

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,28 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
+Rails.application.config.assets.paths << Rails.root.join('vendor','css')
+Rails.application.config.assets.paths << Rails.root.join('vendor','img')
+Rails.application.config.assets.paths << Rails.root.join('vendor','js')
+
+Rails.application.config.assets.paths << Rails.root.join('vendor','vendor','bootstrap','css')
+Rails.application.config.assets.paths << Rails.root.join('vendor','vendor','bootstrap','js')
+
+Rails.application.config.assets.paths << Rails.root.join('vendor','vendor','font-awesome','css')
+Rails.application.config.assets.paths << Rails.root.join('vendor','vendor','font-awesome','fonts')
+Rails.application.config.assets.paths << Rails.root.join('vendor','vendor','font-awesome','less')
+Rails.application.config.assets.paths << Rails.root.join('vendor','vendor','font-awesome','scss')
+
+Rails.application.config.assets.paths << Rails.root.join('vendor','vendor','jquery')
+
+Rails.application.config.assets.paths << Rails.root.join('vendor','vendor','jquery-easing')
+
+Rails.application.config.assets.paths << Rails.root.join('vendor','vendor','simple-line-icons','css')
+Rails.application.config.assets.paths << Rails.root.join('vendor','vendor','simple-line-icons','fonts')
+Rails.application.config.assets.paths << Rails.root.join('vendor','vendor','simple-line-icons','less')
+Rails.application.config.assets.paths << Rails.root.join('vendor','vendor','simple-line-icons','scss')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( *.svg *.jpg *.png *.eot *.ttf *.woff *.woff2 *.otf *.js)
---
+++ vendor/Taller Implementación de Plantilla Bootstrap v4.md
@@ -0,0 +1,38 @@
+# Taller de Integración de una plantilla Bootstrap v4
+
+- La plantilla a implementar  para la realización de esta actividad la encuentras [este repositorio de Github.](https://github.com/DesafioLatam/PlantillaBootstrap4/)
+
+## Ejercicio:
+
+- Descargar la plantilla desde el repositorio de GitHub.
+
+- Crear un nuevo proyecto en Rails.
+
+- Inicializar Git.
+
+- Implementar la plantilla en el proyecto.
+
+- Se evaluará:
+
+    - Cargado de archivos CSS.
+    
+    - Cargado de archivos JS.
+
+    - Cargado de imágenes.
+    
+    - Implementación de tipografías.
+
+- El inspector de elementos (consola) no debe reportar errores(en rojo).
+
+- El proyecto debe ser subido a un repositorio en tu cuenta de GitHub.
+
+
+
+#### Hints :
+
+***- El orden de los archivos en application.js y application.css importa***
+
+***- El archivo new-age.js debe estar cargado al final de la pagina donde aplique su efecto Usando el siguiente helper <%= javascript_include_tag('new-age.min.js') %>***
+
+  
+
